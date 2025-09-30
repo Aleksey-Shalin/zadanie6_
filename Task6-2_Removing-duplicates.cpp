85e5bb66277c34a6d483d8b4e353cc742cf027f3
Aleksey <aleksey_ktotamych@mail.ru>
2025-09-30 15:48:56 +0300

    Add files via upload

---
+++ Task6-2_Removing-duplicates.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+
+int power_of_number(int value, int power)
+{
+	int result = 1;
+	for (int i = 0; i < power; i++)
+	{
+		result *= value;
+	}
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+	return result;
+}
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russian");
+	power_of_number(5, 2);
+	power_of_number(3, 3);
+	power_of_number(4, 4);
+}
