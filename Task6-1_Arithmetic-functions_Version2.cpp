7fc77c342b267defba23bcce536f2847425c7e09
Aleksey <aleksey_ktotamych@mail.ru>
2025-09-30 22:49:41 +0300

    Add files via upload

---
+++ Task6-1_Arithmetic-functions_Version2.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+int sum(int a, int b)
+{
+	return a + b;
+}
+
+int diff(int a, int b)
+{
+	return a - b;
+}
+
+int multiplication(int a, int b)
+{
+	return a * b;
+}
+
+double division(double a, double b)
+{
+	return a / b;
+}
+
+int main(int arg, char** argv)
+{
+	int a = 5;
+	int b = 10;
+	
+	//int s = sum(a, b);
+	//int dif = diff(a, b);
+	//int mult = multiplication(a, b);
+	//double div = static_cast<double>(division(a, b));
+
+	std::cout << a << " + " << b << " = " << sum(a, b) << std::endl;
+	std::cout << a << " - " << b << " = " << diff(a, b) << std::endl;
+	std::cout << a << " * " << b << " = " << multiplication(a, b) << std::endl;
+	std::cout << a << " / " << b << " = " << static_cast<double>(division(a, b)) << std::endl;
+
+	return 0;
+}
+
---
+++ Task6-2_Removing-duplicates(исправленное).cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+void power_of_number(int value, int power)
+{
+	int result = 1;
+	for (int i = 0; i < power; i++)
+	{
+		result *= value;
+	}
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russian");
+	power_of_number(5, 2);
+	power_of_number(3, 3);
+	power_of_number(4, 4);
+}
